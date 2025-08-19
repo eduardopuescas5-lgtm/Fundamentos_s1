3ccfa90e8a64f524e702255155353d7007306447
eduardo <eduardopuescas5@gmail.com>
2025-08-19 15:52:43 -0500

    Mi primera carga a github

---
+++ s1_fund_algor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace s1_fund_algor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi primera carga a github");
+
+        }
+
+
+    }
+}
